53c998c38faf9903b55024d685f9e720b0dd62e3
proudloyaltier (plt) <timediamail@gmail.com>
2018-06-06 18:08:03 -0400

    Add h3 content
    adds coppyable css content.

--- bckg.js
+++ bckg.js
@@ -1,14 +1,13 @@
-var h3 = document.querySelector("h3");
+var h3 = document.querySelector(".h3");
 var c1 = document.querySelector(".color1");
 var c2 = document.querySelector(".color2");
 var body = document.getElementById("gr");
 
-console.log(h3);
-console.log(c1);
-console.log(c2);
+SG();
 
 function SG()  {
 	body.style.background = "linear-gradient(to right, "+ c1.value + ", " + c2.value + ")";
+	h3.textContent = body.style.background + ";";
 }
 
 c1.addEventListener("input", SG)
